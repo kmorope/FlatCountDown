5337fd55daa69506665972943b5b016c9dbb86a1
kmorope <kmorope@gmail.com>
2015-07-06 17:28:56 -0500

    Initial Commit
    Started a new library with a lot of options :d

--- flatCountDown.js
+++ flatCountDown.js
@@ -2,7 +2,8 @@
 // Version 1.01  06/07/2015
 // by Camilo Romero at http://github.com/kmorope
 
- 
+
+
  function flatCountdown(dateTime, id)
     {
         var endDate = new Date(dateTime);
@@ -17,10 +18,7 @@
             var difference = endDate - nowDate;
             if (difference < 0) {
                 clearInterval(timer);
-                $('#'+id+' #day').text("00");
-                $('#'+id+' #hour').text("00");
-                $('#'+id+' #minute').text("00");
-                $('#'+id+' #second').text("00"); 
+                $('#'+id+' #second').text("00");
                 return;
             }
 
@@ -29,13 +27,41 @@
             var mm = Math.floor((difference % hours) / minutes);
             var ss = Math.floor((difference % minutes) / seconds);
 
-            $('#'+id+' #day').text(dd);
-            $('#'+id+' #hour').text(hh);
-            $('#'+id+' #minute').text(mm);
-            if (ss < 10)
-                $('#'+id+' #second').text("0" + ss); 
-            else
-                $('#'+id+' #second').text(ss); 
+            if(dd != 0){
+              $('#'+id+' #day').text(dd);
+            }
+            else {
+              $('#'+id+' #day').hide();
+              $('#'+id+' #ddLabel').hide();
+            }
+
+            if(hh != 0){
+              $('#'+id+' #hour').text(hh);
+            }
+            else {
+              $('#'+id+' #hour').hide();
+              $('#'+id+' #hhLabel').hide();
+            }
+
+            if(mm != 0){
+              $('#'+id+' #minute').text(mm);
+            }
+            else {
+              $('#'+id+' #minute').hide();
+              $('#'+id+' #mmLabel').hide();
+            }
+
+            if(ss != 0){
+              if (ss < 10)
+                  $('#'+id+' #second').text("0" + ss);
+              else
+                  $('#'+id+' #second').text(ss);
+            }
+            else {
+              $('#'+id+' #second').hide(); 
+            }
+
+
         }
 
         timer = setInterval(calcRemainingDate, 1000);
---
+++ flatCountDownLib.js
@@ -0,0 +1,10 @@
+/**
+  * flatCountDown.js
+  *
+  *@author Camilo Romero
+  *@copyright 2015 - JDii Code Lab
+  *@license http://www.opensource.org/licenses/mit-license.php
+  *
+**/
+
+var flatCounter;
